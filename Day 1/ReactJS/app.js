72c378c03cc2e6dfb1fb195458a7b9621e4cfcf7
Tusar <tusarahmed141@gmail.com>
2022-10-18 14:10:48 +0600

    Separet increment using reactjs

--- Day 1/ReactJS/app.js
+++ Day 1/ReactJS/app.js
@@ -1,16 +1,23 @@
 const domContainer = document.getElementById("root");
 
 const Increment = () => {
+  const [increment, setIncrement] = React.useState(0);
   return (
     <>
-      <div class="container">
-        <p id="display">0</p>
+      <div className="container">
+        <p id="display"> {increment} </p>
         <div>
-          <button id="increment-btn">Increment +</button>
+          <button id="increment-btn" onClick={() => setIncrement(increment + 1)}>Increment +</button>
         </div>
       </div>
     </>
   );
 };
 
-ReactDOM.render(<Increment />, domContainer);
+ReactDOM.render(
+  <>
+  <Increment />
+  <Increment />
+  <Increment />
+  </>
+, domContainer);
