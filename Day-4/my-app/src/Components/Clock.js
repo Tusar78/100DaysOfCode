cca5d7e3bf1db4d8f23d856ec69a247a9ed76f7a
Tusar <tusarahmed141@gmail.com>
2022-10-21 14:54:41 +0600

    Watch is ready

--- Day-4/my-app/src/Components/Clock.js
+++ Day-4/my-app/src/Components/Clock.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Clock extends Component {
+  state = { date: new Date() };
+
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
   render() {
     return (
-      <div>
-        
+      <div className="clock">
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
       </div>
-    )
+    );
   }
 }
 
-export default Clock
+export default Clock;
