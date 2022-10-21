8b78bab0537b64fa9d80255d76eacf2e47123894
Tusar <tusarahmed141@gmail.com>
2022-10-21 14:28:56 +0600

    Create a new class componen

--- Day-4/my-app/src/Components/Clock.js
+++ Day-4/my-app/src/Components/Clock.js
@@ -1,32 +1,13 @@
-import React from "react";
+import React, { Component } from 'react'
 
-class Clock extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { date: new Date() };
-  // }
-  state = { date: new Date() };
-
-  componentDidMount() {
-    this.clockTimer = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.clockTimer);
-  }
-
-  tick() {
-    this.setState({ date: new Date() });
-  }
-
+export class Clock extends Component {
   render() {
-    const { locale } = this.props;
     return (
       <div>
-        <h2>Time: {this.state.date.toLocaleTimeString(locale)}</h2>
+        
       </div>
-    );
+    )
   }
 }
 
-export default Clock;
+export default Clock
