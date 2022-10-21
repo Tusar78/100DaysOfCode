50267598a3443f151f7bb1eed2fe1430d7a48afa
Tusar <tusarahmed141@gmail.com>
2022-10-21 14:58:09 +0600

    Updated bengoli clock

--- Day-4/my-app/src/Components/Clock.js
+++ Day-4/my-app/src/Components/Clock.js
@@ -3,20 +3,26 @@ import React, { Component } from "react";
 export class Clock extends Component {
   state = { date: new Date() };
 
-  componentDidMount() {
-    this.clockTimer = setInterval(() => this.tick(), 1000);
-  }
-
   tick() {
     this.setState({
       date: new Date()
     })
   }
 
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+
+
   render() {
+    const { locale } = this.props;
     return (
       <div className="clock">
-        <h2>{this.state.date.toLocaleTimeString()}</h2>
+        <h2>{this.state.date.toLocaleTimeString(locale)}</h2>
       </div>
     );
   }
