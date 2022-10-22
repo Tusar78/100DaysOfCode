c36c2bebe4815c1d4dc982084496ff4fb7ad3c78
Tusar <tusarahmed141@gmail.com>
2022-10-22 13:25:10 +0600

    Added checkbox

--- Day-5/my-app/src/Components/Form.js
+++ Day-5/my-app/src/Components/Form.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 
 export class Form extends Component {
-  state = { title: "", text: "" };
+  state = { title: "", text: "", library: "" };
 
   handleChange = (e) => {
     if (e.target.type === "text") {
       this.setState({
         title: e.target.value,
       });
-    } else if (e.target.type === 'textarea') {
+    } else if (e.target.type === "textarea") {
       this.setState({
         text: e.target.value,
+      });
+    } else if (e.target.type === "select-one") {
+      this.setState({
+        library: e.target.value,
       });
     }
   };
   render() {
-    const { title, text } = this.state;
+    const { title, text, library } = this.state;
     return (
       <div>
         <form>
@@ -31,6 +35,15 @@ export class Form extends Component {
             value={text}
             onChange={this.handleChange}
           ></textarea>
+          <select value={library} onChange={this.handleChange}>
+            <option value="React">React</option>
+            <option value="Angular">Angular</option>
+            <option value="Vue">Vue</option>
+          </select>
+          <div className="checkbox-wrap">
+            <input type="checkbox" id="remember"/>
+            <label htmlFor="remember">Remember Me</label>
+          </div>
         </form>
       </div>
     );
