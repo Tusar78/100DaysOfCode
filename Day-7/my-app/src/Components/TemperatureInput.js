2da0c658298f759dc0acc08e7fdd43d686b72536
Tusar <tusarahmed141@gmail.com>
2022-10-24 22:09:44 +0600

    Convert class component to functional component

--- Day-7/my-app/src/Components/TemperatureInput.js
+++ Day-7/my-app/src/Components/TemperatureInput.js
@@ -1,35 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
 const scaleNames = {
   c: "Celsius",
   f: "Fahrenheit",
 };
 
-export class TemperatureInput extends Component {
-  state = { temperature: "" };
-
-  handleTemperature = e => {
-    this.setState({
-      temperature: e.target.value
-    })
-  }
-
-  render() {
-    const { temperature } = this.state;
-    const { scale } = this.props;
-    return (
-      <fieldset>
-        <legend>Enter Temperature in {scaleNames[scale]}</legend>
-        <input
-          type="text"
-          name="temperature"
-          placeholder="Enter temperature"
-          value={temperature}
-          onChange={this.handleTemperature}
-        />
-      </fieldset>
-    );
-  }
-}
+const TemperatureInput = ({ temperature, handleTemperature, scale }) => {
+  return (
+    <fieldset>
+      <legend>Enter Temperature in {scaleNames[scale]}</legend>
+      <input
+        type="text"
+        name="temperature"
+        placeholder="Enter temperature"
+        value={temperature}
+        onChange={this.handleTemperature}
+      />
+    </fieldset>
+  );
+};
 
 export default TemperatureInput;
