e3a4a997f53249d3e5fea50e734842d56b9080b3
Tusar <tusarahmed141@gmail.com>
2022-10-24 18:11:25 +0600

    ready to our temperature input

--- Day-7/my-app/src/Components/Calculator.js
+++ Day-7/my-app/src/Components/Calculator.js
@@ -1,32 +1,13 @@
 import React, { Component } from "react";
-import BoilingVerdict from "./BoilingVerdict";
+import TemperatureInput from "./TemperatureInput";
 
 export class Calculator extends Component {
-  state = {temperature: ''}
-
-  handleTemperature = e => {
-    this.setState({
-      temperature: e.target.value
-    })
-  }
   render() {
-    const {temperature} = this.state;
     return (
-      <div>
-        <fieldset>
-          <legend>Enter Temperature in celsius.</legend>
-
-          <input
-            type="text"
-            name="text"
-            placeholder="Enter celsius"
-            value={temperature}
-            onChange={this.handleTemperature}
-          />
-        </fieldset>
-
-        { temperature && <BoilingVerdict celsius={parseFloat(temperature)} /> }
-      </div>
+      <>
+        <TemperatureInput scale="f" />
+        <TemperatureInput scale="c" />
+      </>
     );
   }
 }
--- Day-7/my-app/src/Components/TemperatureInput.js
+++ Day-7/my-app/src/Components/TemperatureInput.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
 
 export class TemperatureInput extends Component {
+  state = { temperature: "" };
+
+  handleTemperature = e => {
+    this.setState({
+      temperature: e.target.value
+    })
+  }
+
   render() {
+    const { temperature } = this.state;
+    const { scale } = this.props;
     return (
-      <div>TemperatureInput</div>
-    )
+      <fieldset>
+        <legend>Enter Temperature in {scaleNames[scale]}</legend>
+        <input
+          type="text"
+          name="temperature"
+          placeholder="Enter temperature"
+          value={temperature}
+          onChange={this.handleTemperature}
+        />
+      </fieldset>
+    );
   }
 }
 
-export default TemperatureInput
+export default TemperatureInput;
