f14fb42ca6894349c2c6d25887347f2d999853bc
Tusar <tusarahmed141@gmail.com>
2022-10-24 18:02:44 +0600

    Create a new statefull components called temperature input

---
+++ Day-7/my-app/src/Components/TemperatureInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class TemperatureInput extends Component {
+  render() {
+    return (
+      <div>TemperatureInput</div>
+    )
+  }
+}
+
+export default TemperatureInput
