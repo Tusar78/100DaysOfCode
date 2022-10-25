ff0be4850be82ae04bd133648e94142707dfbac8
Tusar <tusarahmed141@gmail.com>
2022-10-25 23:31:53 +0600

    Prepare to practice composition

--- Day-8/my-app/src/App.js
+++ Day-8/my-app/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Text from './Inheritance/Text';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello World!</h2>
-      <Text />
     </div>
   );
 }
