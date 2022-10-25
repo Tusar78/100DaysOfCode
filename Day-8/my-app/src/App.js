731772f69563ec9cf16b0b52d3d4f140028caab7
Tusar <tusarahmed141@gmail.com>
2022-10-25 23:53:17 +0600

    Last update

--- Day-8/my-app/src/App.js
+++ Day-8/my-app/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Emoji from './Inheritance/Emoji';
+import Text from './Inheritance/Text';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello World!</h2>
+      <br />
+      <br />
+      <Emoji>
+        {
+          ({addEmoji}) => <Text addEmoji={addEmoji} />
+        }
+      </Emoji>
     </div>
   );
 }
---
+++ Day-8/my-app/src/Inheritance/Emoji.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Emoji extends Component {
+  addEmoji = (text, emoji) => `${emoji} ${text} ${emoji}`;
+  render() {
+    const { children } = this.props;
+    return <div>{children({ addEmoji: this.addEmoji })}</div>;
+  }
+}
+
+export default Emoji;
