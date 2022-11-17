bb0c756727ccc74db96daa59f7d893395e800c6c
Tusar <tusarahmed141@gmail.com>
2022-11-17 13:51:41 +0600

    ready

--- Day-10/my-app/src/App.js
+++ Day-10/my-app/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Section from "./Components/Section";
 
 export class App extends Component {
-  state = {
-    theme: "dark",
-  };
   render() {
-    const { theme } = this.state;
     return (
       <>
-        <Section theme={theme} />
+        <h2>Hello World</h2>
       </>
     );
   }
