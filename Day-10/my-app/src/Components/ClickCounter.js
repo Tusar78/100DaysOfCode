a16bf5421c6093981fd4e2b8c775c606077f4201
Tusar <tusarahmed141@gmail.com>
2022-11-17 16:43:24 +0600

    Complete HOC Pattern

--- Day-10/my-app/src/Components/ClickCounter.js
+++ Day-10/my-app/src/Components/ClickCounter.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from 'react';
+import withCounter from '../HOC/withCounter';
 
-export class ClickCounter extends Component {
-  
-  render() {
-    const { count } = this.state;
-    return (
-      <div>
-        <button type="button" onClick={this.incrementCount}>Clicked {count} Times</button>
-      </div>
-    );
-  }
-}
+const ClickCounter = ({count, incrementCount}) => {
+  console.log(count);
+  return (
+    <div>
+      <button onClick={incrementCount}>Click {count} time</button>
+    </div>
+  );
+};
 
-export default ClickCounter;
+export default withCounter(ClickCounter);
