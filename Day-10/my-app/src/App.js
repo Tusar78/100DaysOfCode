84bf646d6bf57b74c420cdd23918e7dadd2d4a6d
Tusar <tusarahmed141@gmail.com>
2022-11-17 23:28:04 +0600

    hover and click function imprement

--- Day-10/my-app/src/App.js
+++ Day-10/my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Click from './Components/Click';
 import User from './Components/User';
+import Hover from './Components/Hover';
 import './App.css'
 
 const App = () => {
   return (
     <div>
       <Click />
+      <Hover />
       <User />
     </div>
   );
