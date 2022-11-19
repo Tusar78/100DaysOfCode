667dd4814aa441ab1430adaea39c2a34613f7cf1
Tusar <tusarahmed141@gmail.com>
2022-11-19 13:15:24 +0600

    Our personal context

--- Day-10/my-app/src/Components/Content.js
+++ Day-10/my-app/src/Components/Content.js
@@ -2,15 +2,27 @@ import React from "react";
 import Counter from "./Counter";
 import Hover from "./Hover";
 
-const Content = ({theme}) => {
+const Content = ({ theme }) => {
   return (
     <div>
       <h2>This is Content Component</h2>
 
       <Counter>
-        {(count, incrementCount) => (
-          <Hover count={count} incrementCount={incrementCount} theme={theme} />
-        )}
+        {(count, incrementCount) => {
+          return (
+            <Consumer>
+              {
+                (value) => (
+                  <Hover
+                    count={count}
+                    incrementCount={incrementCount}
+                    theme={theme}
+                  />
+                )
+              } 
+            </Consumer>
+          )
+        }}
       </Counter>
     </div>
   );
