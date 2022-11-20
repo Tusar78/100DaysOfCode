2ad9da4e273127c321d52cbe9a22f5be03d1764e
Tusar <tusarahmed141@gmail.com>
2022-11-20 15:24:03 +0600

    hook another rules done

--- Day-12/my-app/src/App.js
+++ Day-12/my-app/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Todo from './Components/Todo';
+import HookRules from './Components/HookRules';
 
 const App = () => {
   return (
     <div>
-      <Todo />
+      <HookRules />
     </div>
   );
 };
