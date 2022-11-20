aad40efdb48d90463b01627ad86ac15e2ed36a85
Tusar <tusarahmed141@gmail.com>
2022-11-20 13:09:31 +0600

    todo add on class components

---
+++ Day-12/my-app/src/Components/TodoClass.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export class TodoClass extends Component {
+  state = {
+    todo: "",
+    warning: null,
+  };
+
+  handleInput = (e) => {
+    const inputValue = e.target.value;
+    const warning = inputValue.includes(".js")
+      ? "You need javascript skill to complete the task. Do you have it?"
+      : null;
+
+    this.setState({
+      todo: inputValue,
+      warning,
+    });
+  };
+
+  render() {
+    const { todo, warning } = this.state;
+    return (
+      <>
+        <p>{todo}</p>
+        <p>
+          <textarea name="todo" value={todo} onChange={this.handleInput}></textarea>
+        </p>
+        <hr />
+        <h2>{warning || "Good Choice"}</h2>
+      </>
+    );
+  }
+}
+
+export default TodoClass;
