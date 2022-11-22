ee6013dba84c6852d63be9081908de17bd0e6b9e
Tusar <tusarahmed141@gmail.com>
2022-11-22 11:02:19 +0600

    Use call back

--- Day-13/my-app/src/App.js
+++ Day-13/my-app/src/App.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
-import Button from './Components/Button';
-import ShowCount from './Components/ShowCount';
-import Title from './Components/Title';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import Button from "./Components/Button";
+import ShowCount from "./Components/ShowCount";
+import Title from "./Components/Title";
+import "./App.css";
 
 const App = () => {
   const [count1, setCount1] = useState(0);
   const [count5, setCount5] = useState(0);
 
-  const incrementOne = () => setCount1((prevCount) => prevCount + 1);
-  const incrementFive = () => setCount5((prevCount) => prevCount + 5);
+  const incrementOne = useCallback(() => {
+    setCount1((prevCount) => prevCount + 1);
+  }, []);
+  const incrementFive = useCallback(
+    () => setCount5((prevCount) => prevCount + 5),
+    []
+  );
   return (
     <>
       <Title />
-      <ShowCount count={count1} title='Counter one' />
+      <ShowCount count={count1} title="Counter one" />
       <Button increment={incrementOne}>Increment by One</Button>
       <hr />
-      <ShowCount count={count5} title='Counter two' />
+      <ShowCount count={count5} title="Counter two" />
       <Button increment={incrementFive}>Increment by Five</Button>
     </>
   );
