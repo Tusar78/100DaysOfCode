92727447d7a8b94c6c57880f7a32afa72fe02d85
Tusar <tusarahmed141@gmail.com>
2022-11-22 11:11:30 +0600

    Even odd done

--- Day-13/my-app/src/App.js
+++ Day-13/my-app/src/App.js
@@ -8,9 +8,14 @@ const App = () => {
   const [count1, setCount1] = useState(0);
   const [count5, setCount5] = useState(0);
 
+  const isEvenOrOdd = () => {
+    return count1 % 2 === 0;
+  }
+
   const incrementOne = useCallback(() => {
     setCount1((prevCount) => prevCount + 1);
   }, []);
+
   const incrementFive = useCallback(
     () => setCount5((prevCount) => prevCount + 5),
     []
@@ -19,6 +24,7 @@ const App = () => {
     <>
       <Title />
       <ShowCount count={count1} title="Counter one" />
+      <p>{isEvenOrOdd() ? 'Even' : 'Odd'}</p>
       <Button increment={incrementOne}>Increment by One</Button>
       <hr />
       <ShowCount count={count5} title="Counter two" />
