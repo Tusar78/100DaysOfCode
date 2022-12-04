fc42340ae7ccc3dd3657c137cd60ae19d578951d
Tusar <tusarahmed141@gmail.com>
2022-12-04 23:04:24 +0600

    Last update

--- Day-14/my-app/src/App.js
+++ Day-14/my-app/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import GlobalStyle from './Components/Styles/GlobalStyle.styles';
 import Tutorial from './Components/Tutorial';
 
 const App = () => {
-  return <Tutorial />
+  return (
+    <>
+      <GlobalStyle />
+      <Tutorial />
+    </>
+  )
 };
 
 export default App;
