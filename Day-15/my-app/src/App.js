e4ae173d3f2faa1cfca1027b8e2af3de0c02479b
Tusar <tusarahmed141@gmail.com>
2022-12-05 12:17:59 +0600

    Implement navling

--- Day-15/my-app/src/App.js
+++ Day-15/my-app/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Service from "./Components/Service";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/service" component={Service} />
+      </BrowserRouter>
+    </>
   );
 };
 
--- Day-15/my-app/src/Components/Navbar.js
+++ Day-15/my-app/src/Components/Navbar.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      This is navbar
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/" activeStyle={{fontWeight: 'bold', color: 'red'}}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/about" activeStyle={{fontWeight: 'bold', color: 'red'}}>About</NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/service" activeStyle={{fontWeight: 'bold', color: 'red'}}>Services</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
