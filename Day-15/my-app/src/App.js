0491dffbced689a9631409a3d2dfe00aba2d2ecb
Tusar <tusarahmed141@gmail.com>
2022-12-05 12:30:30 +0600

    Set a not found page

--- Day-15/my-app/src/App.js
+++ Day-15/my-app/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import About from "./Components/About";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import Service from "./Components/Service";
+import PageNotFound from "./Components/PageNotFound";
 import "./App.css";
 
 const App = () => {
@@ -11,10 +12,14 @@ const App = () => {
     <>
       <BrowserRouter>
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/service" component={Service} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/service" component={Service} />
+
+          <Route exact component={PageNotFound} />
+        </Switch>
       </BrowserRouter>
     </>
   );
---
+++ Day-15/my-app/src/Components/PageNotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      <p>This is error page</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
